9d7892ffff02ee524f55937454e0ef49211e51c4
HaoTangzhou <haotangzhou@sina.com>
2023-12-08 19:47:19 -0800

    import jsonify in run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, jsonify, render_template, request, send_from_directory
 from Engine.nosql import NoSQL
 from Engine.relational import Relational
 import re
